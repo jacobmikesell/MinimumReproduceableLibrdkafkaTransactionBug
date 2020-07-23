70bc0d8b4d8ff1dabf36c59301dcb8479659f3c7
jacobmikesell <63617254+jacobmikesell@users.noreply.github.com>
2020-07-23 12:31:04 -0700

    Add files

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+const TopicName = "test"
+
+func main() {
+	config:= &kafka.ConfigMap{"bootstrap.servers": "localhost",
+		"isolation.level": "read_committed",
+		"enable.idempotence": true,
+		"enable.auto.commit": false,
+		"enable.auto.offset.store": false,
+		"transactional.id": "testTransactionId",
+		"broker.address.family": "v4",
+	}
+
+	producer, err := kafka.NewProducer(config)
+	if err != nil {
+		panic("Failed to make producer")
+	}
+	defer producer.Close()
+
+	topic:= TopicName
+
+	err = createTopicIfNecessary(config, topic)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, cxl := context.WithTimeout(context.Background(), 3000)
+	defer cxl()
+	err = producer.InitTransactions(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			evt, ok := <-producer.Events()
+			if !ok{
+				return
+			}
+			msg, ok := evt.(*kafka.Message)
+			if !ok {
+				fmt.Println(ok)
+
+			}
+			if msg.TopicPartition.Error != nil {
+				fmt.Println(msg.TopicPartition)
+			}
+		}
+	}()
+
+	err = producer.BeginTransaction()
+	if err != nil {
+		panic(err)
+	}
+
+
+	i := int64(0)
+	for i< 40 *1000 *1000 {
+		i++
+		buf := make([]byte, binary.MaxVarintLen64)
+		binary.PutVarint(buf, i)
+		msg := &kafka.Message{Value: buf, Key: buf, TopicPartition: kafka.TopicPartition{Partition: 0, Topic: &topic, Offset: kafka.OffsetBeginning}}
+		err = producer.Produce(msg, nil)
+		if err != nil {
+			panic(err)
+		}
+		if i%1000000 == 0 {
+			fmt.Println("progress update emitted", i)
+		}
+	}
+	fmt.Println("Emitted 40 million events but did not commit them")
+}
+
+func createTopicIfNecessary(config *kafka.ConfigMap, topic string) error {
+	admin, err := kafka.NewAdminClient(config)
+	if err != nil {
+		return err
+	}
+	defer admin.Close()
+	md, err := admin.GetMetadata(nil, true, 5000)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := md.Topics[topic]; !ok {
+		topics, err := admin.CreateTopics(context.Background(), []kafka.TopicSpecification{
+			{
+				Topic:             topic,
+				NumPartitions:     1,
+				ReplicationFactor: 1,
+				Config:            make(map[string]string),
+			},
+		})
+		if err != nil {
+			return  err
+		}
+		if topics[0].Error.Code() != kafka.ErrNoError {
+			return topics[0].Error
+		}
+	}
+	return nil
+}
